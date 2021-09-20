8c98867ea403cd11ef2e7aea525fea8731102ec0
OksanaZhuravel <opuseva@mail.ru>
2021-09-20 18:12:18 +0300

    project finished

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,24 @@
-const gulp = require("gulp");
-const plumber = require("gulp-plumber");
-const sourcemap = require("gulp-sourcemaps");
-const less = require("gulp-less");
-const postcss = require("gulp-postcss");
-const autoprefixer = require("autoprefixer");
-const sync = require("browser-sync").create();
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const sourcemap = require('gulp-sourcemaps');
+const less = require('gulp-less');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const sync = require('browser-sync').create();
 
 // Styles
 
 const styles = () => {
-  return gulp.src("source/less/style.less")
+  return gulp
+    .src('source/less/style.less')
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(less())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
-    .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemap.write('.'))
+    .pipe(gulp.dest('source/css'))
     .pipe(sync.stream());
-}
+};
 
 exports.styles = styles;
 
@@ -28,24 +27,22 @@ exports.styles = styles;
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'source',
     },
     cors: true,
     notify: false,
     ui: false,
   });
   done();
-}
+};
 
 exports.server = server;
 
 // Watcher
 
 const watcher = () => {
-  gulp.watch("source/less/**/*.less", gulp.series("styles"));
-  gulp.watch("source/*.html").on("change", sync.reload);
-}
+  gulp.watch('source/less/**/*.less', gulp.series('styles'));
+  gulp.watch('source/*.html').on('change', sync.reload);
+};
 
-exports.default = gulp.series(
-  styles, server, watcher
-);
+exports.default = gulp.series(styles, server, watcher);
